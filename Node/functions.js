0529de6fac3fc2ff244be6c73049156575e63590
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-09 11:45:01 +0300

    New functions have been created

--- Node/functions.js
+++ Node/functions.js
@@ -5,29 +5,36 @@ function f(x) {
 console.log(f(34));
 
 console.log('-------------------');
-
-function topla(a, b) {
-    return a + b;
+function addition (x,y) {
+    return x+y;
 }
 
-console.log(topla(34, 90));
+console.log("Result of the operation: "+addition(23,54));
+
+console.log('-------------------');
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+console.log("Result of the operation: "+multiplication(34, 90));
 
 console.log('-------------------');
 
 function message() {
-    console.log('new is always well');
+    console.log('new is always better');
 };
 
 message();
 
 console.log('-------------------');
 
-function fact(n) {
+function fact(n) {//Recursive
     if (n == 0)
         return 1;
     return n * fact(n - 1);
 }
+console.log("Result of the factorial: "+fact(7));
 
-console.log(fact(7));
 console.log('-------------------');
 setTimeout(message, 4000);//after waiting 4 seconds, runs the code
