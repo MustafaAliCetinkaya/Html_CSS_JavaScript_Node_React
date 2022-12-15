d9ae14e7e99d973e2c2fe3983315251fe5bc6c4a
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-16 01:16:45 +0300

    Express and Jason packages are installed

--- Node/Express.js
+++ Node/Express.js
@@ -1,5 +1,22 @@
 var express=require('express');
-var app=express;
+var app=express();
+
+var fs=require('fs');
+
+app.get('/list',function (request,response) {
+    //response.send('Calling which list the users');
+    fs.readFile('users.json','utf-8',function (err,data){
+        console.log(data)
+        response.end(data);
+    });
+});
+app.get('/add',function (request,response) {
+    response.end("Calling which add the users");
+});
+app.get("/remove",function (request,response) {
+    response.end("Calling which remove the users");
+});
+
 
 var server=app.listen(8888, function () {
     console.log("Sunucu çalışıyor");
