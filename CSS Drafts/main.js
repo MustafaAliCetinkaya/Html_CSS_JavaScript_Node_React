6da43f6ca4fe8fb0a3b6b4cc58fcc6fcc268eddc
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-23 15:55:18 +0300

    New Javascript projects have been uploaded

---
+++ CSS Drafts/main.js
@@ -0,0 +1,22 @@
+var a=100;
+let b=300;
+var c=a+b;
+console.log(c)
+
+typeof c
+c=true
+typeof c
+a="ali"
+typeof a
+b=3.45
+typeof b
+
+
+alert("This is a test");
+
+let pupUp=prompt("What is your name?","Your name please:");
+console.log(pupUp);
+
+console.log(document)
+console.dir(document)
+
---
+++ Introduction to JavaScript/Variables.js
@@ -0,0 +1,22 @@
+var a=100
+let b=300//usually is used in blocks
+let c=a+b;
+console.log(c)
+console.log(typeof a);
+b="alican"
+console.log(b)
+console.log(typeof b);
+c=null
+console.log(typeof c);
+console.log("---------------------")
+let z="120"
+console.log(typeof z)
+console.log(Number(z))
+console.log(typeof Number(z))
+console.log("---------------------")
+let ax=""+45+98
+console.log(ax)
+let h="54"*"10"
+console.log(h)
+
+
