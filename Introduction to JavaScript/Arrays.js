67c570f974fd127ea5bdf56a46c23a3954b7412a
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-23 16:34:02 +0300

    New Javascript projects have been uploaded

--- Introduction to JavaScript/Arrays.js
+++ Introduction to JavaScript/Arrays.js
@@ -17,4 +17,15 @@ console.log("-----------------------------------------");
 const numberx=[1,1,1,2,2,2,3,4,5,6,7,7,8,9];
 const uniqueNumbers2=[...new Set(numberx)];
 console.log(uniqueNumbers2);
+console.log("-----------------------------------------");
+var myArray=[false,34,"First"]
+console.log(myArray.length);
+myArray[5]="Alican"
+console.log(myArray.length)
+console.log(myArray)
+myArray[5]+=" Kayahan "
+console.log(myArray)
+console.log(myArray[0]+" / "+myArray[5])
+
+
 
---
+++ Introduction to JavaScript/Object.js
@@ -0,0 +1,14 @@
+let myObject={}
+myObject.name="Ali";
+myObject.surname="Kaya";
+console.log(myObject)
+myObject.surname="Hayhuy"
+console.log(myObject)
+console.log("--------------------------")
+var myJob={jobtitle:"Teacher",salary:12000,isFullTime:true}
+console.log(myJob.isFullTime)
+console.log(myJob.jobtitle)
+console.log(myJob.salary)
+console.log(myJob)
+console.log("--------------------------")
+console.log(document.lastModified);
