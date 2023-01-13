cd6879cbf1b4156b7c7c1163db84a764ae31ece8
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-13 21:14:52 +0300

    Javascript Fetch projects have been uploaded

--- JavaScriptFundamentals/FetchScript.js
+++ JavaScriptFundamentals/FetchScript.js
@@ -1,4 +1,28 @@
+const output = document.getElementById('output');
+const clickMe = document.getElementById('clickMe');
+clickMe.addEventListener('click',myJSON);
 
+function myJSON(){
+    console.log('click');
+    const url = 'https://randomuser.me/api/?results=50';
+    fetch(url).then(function(response){
+        return response.json();
+    }).then(function(data){
+        let html = '';
+        data.results.forEach(function(person){
+            console.log(person);
+            //html += '<div>'+person.email+'</div>';
+            html += '<div>'+person.name.first+'</div>';
+        });
+        output.innerHTML = html;
+    }).catch(function(e){
+    })
+}
+
+
+
+//Another Application
+/*
 const output = document.getElementById('output');
 const clickMe = document.getElementById('clickMe');
 clickMe.addEventListener('click',myJSON);
@@ -11,8 +35,8 @@ function myJSON(){
     }).then(function(data){
         console.log(data);
         console.log(data.results[0].name.first);
-        //console.log(data.results[0].dob.age);
+        console.log(data.results[0].dob.age);
     }).catch(function(e){
         console.log(e);
     })
-}
+}*/
