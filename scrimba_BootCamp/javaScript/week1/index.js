2c6054ae3271b4a9cd8357d201214f2f2337532d
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-30 23:50:29 +0300

    Scrimba js projects have been uploaded

--- scrimba_BootCamp/javaScript/week1/index.js
+++ scrimba_BootCamp/javaScript/week1/index.js
@@ -1,1 +1,12 @@
-document.getElementById("count-el").innerText = 5+'\nAwesome';
+let count=0;
+
+function increment() {
+    count+=1;
+    document.getElementById("count-el").innerText = count;
+}
+
+function decrement() {
+    count-=1;
+    document.getElementById("count-el").innerText = count;
+}
+
