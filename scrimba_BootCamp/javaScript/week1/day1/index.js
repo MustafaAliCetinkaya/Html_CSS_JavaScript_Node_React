fbab1b1836453fefbb6f644f28fee84546345c85
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-31 12:25:54 +0300

    Scrimba js projects have been uploaded

--- scrimba_BootCamp/javaScript/week1/day1/index.js
+++ scrimba_BootCamp/javaScript/week1/day1/index.js
@@ -1,12 +1,38 @@
-let count=0;
+let count = 0;
 
 function increment() {
-    count+=1;
+    count += 1;
     document.getElementById("count-el").innerText = count;
+
+    if (count > 20) {
+        document.getElementById("count-el").style.background = "yellow";
+    }
+    if (count<0) {
+        document.getElementById("count-el").style.background = "lightgreen";
+    }
+    if (count<=20&&count>0) {
+        document.getElementById("count-el").style.background = "lightblue";
+    }
+    if(count===0){
+        document.getElementById("count-el").style.background = "none";
+    }
 }
 
 function decrement() {
-    count-=1;
+    count -= 1;
     document.getElementById("count-el").innerText = count;
+
+    if (count > 20) {
+        document.getElementById("count-el").style.background = "yellow";
+    }
+    if (count<0) {
+        document.getElementById("count-el").style.background = "lightgreen";
+    }
+    if (count<=20&&count>0) {
+        document.getElementById("count-el").style.background = "lightblue";
+    }
+    if(count===0){
+        document.getElementById("count-el").style.background = "none";
+    }
 }
 
